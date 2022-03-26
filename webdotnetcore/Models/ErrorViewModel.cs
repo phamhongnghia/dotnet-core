75528c595a2a85959b7f1f20f779b1b23652cce8
nghia pham <phamhongnghia13579@gmail.com>
2022-03-26 21:20:52 +0700

    dotnet core

---
+++ webdotnetcore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webdotnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
